dd97cd089ee821daa49b2d0de1e9c5153dddb017
Tyler Smith <tylersmith.me@gmail.com>
2016-10-11 11:23:28 -0700

    FEATURE: Track each step of provisioning.

--- main.go
+++ main.go
@@ -3,202 +3,88 @@ package main
 import (
 	"crypto/tls"
 	"database/sql"
-	"encoding/json"
 	"errors"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/gocraft/health"
-	"github.com/gocraft/web"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// CORS headers
-const accessControlAllowOriginHeader = "*"
-const accessControlAllowHeadersHeader = "Origin, X-Requested-With, Content-Type, Accept"
+// nodeTableSchema is a SQL statement that creates the logging table
+const nodeTableSchema = `CREATE TABLE IF NOT EXISTS nodes (
+  ip TEXT NOT NULL,
+  state TEXT NOT NULL,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
+  PRIMARY KEY(ip, state)
+  );`
 
 // HTTPTimeout is the amount of time to wait for a read/write timeout on the request
 var HTTPTimeout = 15 * time.Second
 
 // HTTPClient is a custom HTTP client that doesn't check tls signature chains
-var HTTPClient = &http.Client{Transport: &http.Transport{
-	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-}}
+var HTTPClient = &http.Client{
+	Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	},
+	Timeout: HTTPTimeout,
+}
 
 // stream is a health.Stream used for instrumentation
-var stream = newStream()
-
-// middlewareFunc is a gocraft/web compatible middleware
-type middlewareFunc func(c *Context, rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc)
-
-// Context is the context for incoming HTTP requests
-type Context struct {
-	job        *health.Job
-	err        error
-	nodeStatus string
-	nodeIP     string
-}
-
-// StatusResponse represents the response from the ob-relay status endpoint
-type StatusResponse struct {
-	Status string `json:"status"`
-}
-
-// AddCORSHeaders sets the proper HTTP response headers for a CORS request
-func (*Context) AddCORSHeaders(rw web.ResponseWriter, r *web.Request, next web.NextMiddlewareFunc) {
-	rw.Header().Set("Access-Control-Allow-Origin", accessControlAllowOriginHeader)
-	rw.Header().Set("Access-Control-Allow-Headers", accessControlAllowHeadersHeader)
-	next(rw, r)
-}
-
-// HealthCheck
-func (c *Context) HealthCheck(rw web.ResponseWriter, r *web.Request, next web.NextMiddlewareFunc) {
-	// Setup instrumentation
-	c.job = stream.NewJob(r.URL.String())
-
-	// Execute the request
-	next(rw, r)
-
-	// We're done if no errors
-	if c.err == nil {
-		c.job.Complete(health.Success)
-		return
-	}
-
-	// Otherwise return the errors to the caller
-	rw.Header().Set("Content-Type", "application/json")
-	rw.WriteHeader(http.StatusInternalServerError)
-	fmt.Fprintf(rw, `{"error":%q}`, c.err)
-	c.job.Complete(health.Error)
-}
-
-// StatusRequestProxyHandler gets a status from ob-relay
-func (c *Context) StatusRequestProxyHandler(rw web.ResponseWriter, r *web.Request) {
-	url := "https://" + r.PathParams["ip"] + ":8080/status"
-
-	// Perform the request
-	resp, err := HTTPClient.Get(url)
-	if err != nil {
-		c.err = err
-		c.job.EventErr("proxy.request_url", c.err)
-		return
-	}
-
-	if resp.StatusCode != 200 {
-		c.err = fmt.Errorf("Error in HTTP request: %d", resp.StatusCode)
-		c.job.EventErr("proxy.request_url", c.err)
-		return
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		c.err = err
-		c.job.EventErr("proxy.read_body", c.err)
-		return
-	}
-
-	status := &StatusResponse{}
-	err = json.Unmarshal(body, status)
-	if err != nil {
-		c.err = err
-		c.job.EventErr("proxy.parse_body", c.err)
-		return
-	}
-
-	c.nodeStatus = status.Status
-	c.nodeIP = r.PathParams["ip"]
-
-	_, err = rw.Write(body)
-	if err != nil {
-		c.err = err
-		c.job.EventErr("proxy.write_body", c.err)
-		return
-	}
-}
-
-func newUpdateNodeStateMiddleware(db *sql.DB) (middlewareFunc, error) {
-	return func(c *Context, rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
-		// Execute handler
-		next(rw, req)
-
-		// Update state
-		update := "INSERT OR REPLACE INTO nodes (ip, state, created_at) values(?, ?, CURRENT_TIMESTAMP)"
-		stmt, err := db.Prepare(update)
-		defer stmt.Close()
-		if err != nil {
-			c.job.EventErr("update_node_state.prepare", err)
-			return
-		}
-
-		_, err = stmt.Exec(c.nodeIP, c.nodeStatus)
-		if err != nil {
-			c.job.EventErr("update_node_state.execute", err)
-			return
-		}
-	}, nil
-}
+var stream *health.Stream
 
 func main() {
+	// Create health stream
+	stream = health.NewStream()
+	stream.AddSink(&health.WriterSink{os.Stdout})
+
 	// Get host and port to bind to
 	port := getOSEnvString("CORS_PROXY_PORT", "8080")
 	host := getOSEnvString("CORS_PROXY_HOST", "127.0.0.1")
 	dbFile := getOSEnvString("CORS_PROXY_DB_FILE", "/opt/corsproxy.db")
 
-	// Create DB
-	db, err := sql.Open("sqlite3", dbFile)
+	// Open DB and create logging middleware
+	db, err := openDB(dbFile)
 	if err != nil {
-		stream.EventErrKv("open_db.connect", err, health.Kvs{"file": dbFile})
-		return
-	}
-	if db == nil {
-		err = errors.New("db is nil")
-		stream.EventErrKv("open_db.connect", err, health.Kvs{"file": dbFile})
+		stream.EventErrKv("open_db", err, health.Kvs{"file": dbFile})
 		return
 	}
 
-	// Create table if not exists
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS nodes (
-		ip TEXT NOT NULL PRIMARY KEY,
-		state TEXT,
-		created_at DATETIME
-		);`)
-	if err != nil {
-		stream.EventErrKv("create_table", err, health.Kvs{"file": dbFile})
-		return
-	}
-
-	// Create logging middleware
-	UpdateNodeStateMiddleware, err := newUpdateNodeStateMiddleware(db)
+	updateNodeStateMiddleware, err := newUpdateNodeStateMiddleware(db)
 	if err != nil {
 		stream.EventErr("new_log_middleware", err)
 		return
 	}
 
 	// Create a router to the proxy request handler
-	router := newRouter(UpdateNodeStateMiddleware)
+	router := newRouter(updateNodeStateMiddleware)
 
 	// Start listening
 	stream.EventKv("server_listening", health.Kvs{"host": host, "port": port})
 	http.ListenAndServe(host+":"+port, router)
 }
 
-func newRouter(UpdateNodeStateMiddleware middlewareFunc) *web.Router {
-	return web.New(Context{}).
-		Middleware((*Context).HealthCheck).
-		Middleware(web.LoggerMiddleware).
-		Middleware(web.ShowErrorsMiddleware).
-		Middleware((*Context).AddCORSHeaders).
-		Middleware(UpdateNodeStateMiddleware).
-		Get("/status/:ip", (*Context).StatusRequestProxyHandler)
-}
+// openDB opens a sqlite connection and creates the database/schema if it
+// doesn't exist yet
+func openDB(dbFile string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		return nil, err
+	}
+	if db == nil {
+		err = errors.New("db is nil")
+		return nil, err
+	}
 
-func newStream() *health.Stream {
-	s := health.NewStream()
-	s.AddSink(&health.WriterSink{os.Stdout})
-	return s
+	// Create table if not exists
+	_, err = db.Exec(nodeTableSchema)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 // getOSEnvString returns the environment variable with the given name or the
---
+++ routing.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gocraft/health"
+	"github.com/gocraft/web"
+)
+
+// CORS headers
+const accessControlAllowOriginHeader = "*"
+const accessControlAllowHeadersHeader = "Origin, X-Requested-With, Content-Type, Accept"
+
+// middlewareFunc is a gocraft/web compatible middleware
+type middlewareFunc func(c *Context, rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc)
+
+// Context is the context for incoming HTTP requests
+type Context struct {
+	job        *health.Job
+	err        error
+	nodeStatus string
+}
+
+// StatusResponse represents the response from the ob-relay status endpoint
+type StatusResponse struct {
+	Status string `json:"status"`
+}
+
+func newRouter(UpdateNodeStateMiddleware middlewareFunc) *web.Router {
+	return web.New(Context{}).
+		Middleware((*Context).HealthCheck).
+		Middleware(web.LoggerMiddleware).
+		Middleware(web.ShowErrorsMiddleware).
+		Middleware((*Context).AddCORSHeaders).
+		Middleware(UpdateNodeStateMiddleware).
+		Get("/status/:ip", (*Context).StatusRequestProxyHandler)
+}
+
+// AddCORSHeaders sets the proper HTTP response headers for a CORS request
+func (*Context) AddCORSHeaders(rw web.ResponseWriter, r *web.Request, next web.NextMiddlewareFunc) {
+	rw.Header().Set("Access-Control-Allow-Origin", accessControlAllowOriginHeader)
+	rw.Header().Set("Access-Control-Allow-Headers", accessControlAllowHeadersHeader)
+	next(rw, r)
+}
+
+// HealthCheck
+func (c *Context) HealthCheck(rw web.ResponseWriter, r *web.Request, next web.NextMiddlewareFunc) {
+	// Setup instrumentation
+	c.job = stream.NewJob(r.URL.String())
+
+	// Execute the request
+	next(rw, r)
+
+	// We're done if no errors
+	if c.err == nil {
+		c.job.Complete(health.Success)
+		return
+	}
+
+	// Otherwise return the errors to the caller
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprintf(rw, `{"error":%q}`, c.err)
+	c.job.Complete(health.Error)
+}
+
+// StatusRequestProxyHandler gets a status from ob-relay
+func (c *Context) StatusRequestProxyHandler(rw web.ResponseWriter, r *web.Request) {
+	url := "https://" + r.PathParams["ip"] + ":8080/status"
+
+	// Perform the request
+	resp, err := HTTPClient.Get(url)
+	if err != nil {
+		c.err = err
+		c.job.EventErr("proxy.request_url", c.err)
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		c.err = fmt.Errorf("Error in HTTP request: %d", resp.StatusCode)
+		c.job.EventErr("proxy.request_url", c.err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		c.err = err
+		c.job.EventErr("proxy.read_body", c.err)
+		return
+	}
+
+	status := &StatusResponse{}
+	err = json.Unmarshal(body, status)
+	if err != nil {
+		c.err = err
+		c.job.EventErr("proxy.parse_body", c.err)
+		return
+	}
+
+	c.nodeStatus = status.Status
+
+	_, err = rw.Write(body)
+	if err != nil {
+		c.err = err
+		c.job.EventErr("proxy.write_body", c.err)
+		return
+	}
+}
+
+func newUpdateNodeStateMiddleware(db *sql.DB) (middlewareFunc, error) {
+	return func(c *Context, rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+		c.nodeStatus = "INSTALLING_OPENBAZAAR_RELAY"
+
+		// Execute handler
+		next(rw, req)
+
+		// Update state
+		updateStmt, err := db.Prepare(`
+      WITH new (ip, state) AS ( VALUES(?, ?) )
+      INSERT OR REPLACE INTO nodes (ip, state, updated_at, created_at)
+      SELECT new.ip, new.state, CURRENT_TIMESTAMP, COALESCE(old.created_at, CURRENT_TIMESTAMP)
+      FROM new
+        LEFT JOIN nodes AS old
+        ON new.ip = old.ip AND new.state = old.state
+      LIMIT 1;
+    `)
+		defer updateStmt.Close()
+		if err != nil {
+			c.job.EventErr("update_node_state.prepare", err)
+			return
+		}
+
+		_, err = updateStmt.Exec(req.PathParams["ip"], c.nodeStatus)
+		if err != nil {
+			c.job.EventErr("update_node_state.execute", err)
+			return
+		}
+	}, nil
+}
